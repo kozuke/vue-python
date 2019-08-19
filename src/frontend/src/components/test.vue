3a69146a36a8f6128850edd845a88422ee0da975
kota ishizuka <kota.ishizuka1210@gmail.com>
2019-08-20 00:04:16 +0900

    test element ui

--- src/frontend/src/components/test.vue
+++ src/frontend/src/components/test.vue
@@ -1,3 +1,20 @@
 <template>
-    <input type='text' value="test">
+  <el-input placeholder="Please input" v-model="input">
+  </el-input>
 </template>
+
+<script lang="ts">
+  import {
+    Component,
+    Vue,
+  } from 'vue-property-decorator';
+  import {
+    Input,
+  } from 'element-ui';
+
+  Vue.use(Input);
+  @Component
+  export default class UseElement extends Vue {
+    private input: string = '';
+  }
+</script>
--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="about">
     <test msg="test"/>
+    <test msg="test"/>
+    <test msg="test"/>
+    <test msg="test"/>
     <test2 msg="test2"/>
+    <test2 msg="test2"/>
+    <test2 msg="test2"/>
+    <test2 msg="test2"/>
+    <test3 msg="test3"/>
+    <test3 msg="test3"/>
+    <test3 msg="test3"/>
+    <test3 msg="test3"/>
   </div>
 </template>
 
@@ -9,11 +19,13 @@
 import { Component, Vue } from 'vue-property-decorator';
 import test from '@/components/test.vue'; // @ is an alias to /src
 import test2 from '@/components/test2.vue'; // @ is an alias to /src
+import test3 from '@/components/test3.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     test,
     test2,
+    test3,
   },
 })
 export default class About extends Vue {}
